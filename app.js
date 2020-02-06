97e557192fd6edc692e0058489eb52af423f5e62
eithinzarko <eithinzarko.tk@gmail.com>
2020-02-06 17:02:06 +0630

    text

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": "မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်",
+      "text":"မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်",
       "quick_replies":[
         {
           "content_type":"text",
@@ -149,9 +149,20 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်`
+      "text":"မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်"
     }
   }
+  else if (received_message.text == "Red") {
+      response = {
+        "text":'You like red color' 
+      }
+  }
+  else if (received_message.text == "Green") {
+      response = {
+        "text":'You like green color' 
+      }
+  }  
+
    else if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
