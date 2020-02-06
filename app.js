4457bf6f33712b7b853a9243970fa690b13e65ac
eithinzarko <eithinzarko.tk@gmail.com>
2020-02-06 16:29:27 +0630

    test

--- app.js
+++ app.js
@@ -129,13 +129,27 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `Min Ga Lar Par Sint!မင်္ဂလာပါ`
+      "text": `မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်`,
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/red.png"
+      },{
+        "content_type":"text",
+        "title":"Green",
+        "payload":"<POSTBACK_PAYLOAD>",
+        "image_url":"http://example.com/img/green.png"
+      }
+    ]
+  }
     }
   }else if (received_message.text == "hello") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `Hello!!!!!!!!!`
+      "text": `မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်`
     }
   }
    else if (received_message.text) {    
