eec7c0a1e343c966efbbcdc2dbbf97cc972279f5
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-22 09:45:18 +0630

    renew

--- app.js
+++ app.js
@@ -259,29 +259,30 @@ function handlePostback(sender_psid, received_postback) {
   } else if (payload === 'no') {
     response = { "text": "အားးးနောက်တစ်ပုံပြန်ပို့ပေးပါနော်" }
   }else if (payload === 'getstarted') {
-    response = { "attachment": {
-                  "type": "template",
-                  "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                      "title": "မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ်",
-                      "subtitle": "ဘာများအလိုရှိပါသလဲ?",
-                      "buttons": [
-                        {
-                          "type": "postback",
-                          "title": "တံခါးရွက်ဒီဇိုင်းများ",
-                          "payload": "sstgym",
-                        },
-                        {
-                          "type": "postback",
-                          "title": "ဒီဇိုင်းပေးမည်",
-                          "payload": "gd",
-                        }
-                      ],
-                    }]
+  let response 1 = {"text" : "မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
+  let response 2 = { "attachment": {
+                      "type": "template",
+                      "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                          "title": "မင်္ဂလာပါ! NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ",
+                          "subtitle": "ဘာများအလိုရှိပါသလဲ?",
+                          "buttons": [
+                            {
+                              "type": "postback",
+                              "title": "တံခါးရွက်ဒီဇိုင်းများ",
+                              "payload": "sstgym",
+                            },
+                            {
+                              "type": "postback",
+                              "title": "ဒီဇိုင်းပေးမည်",
+                              "payload": "gd",
+                            }
+                          ],
+                        }]
+                      }
+                    }
                   }
-                }
-              }
   }else if (payload === 'gd') {
     response = { "text": "Please sent image." }
   }else if (payload === 'sstgym') {
