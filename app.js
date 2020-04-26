ba289b44c0c82d718c3e697946060d2e38b93f17
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-26 19:17:11 +0630

    renew

--- app.js
+++ app.js
@@ -442,13 +442,7 @@ function handlePostback(sender_psid, received_postback) {
         });
       });
     }
-else if (received_message.payload === "dtans2") {
-  response = {"text":"ဟုတ်ကဲ့ လူကြီးမင်းမှာယူလိုတဲ့ပုံလေးပို့ပေးပါနော်"}; 
-}
-  
-else if (received_message.payload === 'dtans3') {
-  response = {"text":"ယခုလိုထပ်မံဖြေကြားပေးတဲ့အတွက် ကျေးဇူးတင်ပါတယ်ခင်ဗျာ"};
-}
+
 
 
 
