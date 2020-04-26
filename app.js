c92a9cc3f3a68b1ba11148f66d7687046bc98cb1
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-26 10:33:06 +0630

    renew

--- app.js
+++ app.js
@@ -150,33 +150,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
   let response1 = {"text": "မင်္ဂလာပါ!. NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
-  let response2 = {"text": "လူကြီးမင်းသိလိုသည်များကို အောက်ပါခလုတ်များနှိပ်၍ သိရှိနိုင်ပါတယ်...NS Doors & Windows Shop မှ ကျေးဇူးအထူးတင်ရှိပါတယ်ခင်ဗျာ..."};
-  let response3 = {
-          "text":'လူကြီးမင်းသိလိုသည်များကို အောက်ပါခလုတ်များနှိပ်၍ သိရှိနိုင်ပါတယ်...NS Doors & Windows Shop မှ ကျေးဇူးအထူးတင်ရှိပါတယ်ခင်ဗျာ...',
-          "quick_replies":[
-        {
-          "content_type":"text",
-          "title":"ဆိုင်လိပ်စာ",
-          "payload":"s_address"
-        },{
-          "content_type":"text",
-          "title":"ဆိုင်ဖုန်းနံပါတ်",
-          "payload":"s_Ph"
-        },{
-          "content_type":"text",
-          "title":"တံခါးရွက်ဒီဇိုင်းများကြည့်မည်/မှာမည်",
-          "payload":"L&O"
-        }
-      ]
-    };
-
-callSend(sender_psid, response1).then(()=>{
-  return callSend(sender_psid,response2).then(()=>{
-      return callSend(sender_psid, response3);
-      });  
-  });  
-} 
-  }
+}
 else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
