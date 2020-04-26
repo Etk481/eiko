d0f7b65285677b16242151b397dc501ba17e3816
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-26 10:41:51 +0630

    renew

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text == "Hi" || received_message.text == "hi" || received_message.text == "Hello" || received_message.text == "hello") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-  let response1 = {"text": "မင်္ဂလာပါ!. NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
+  response1 = {"text": "မင်္ဂလာပါ!. NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
   callSend(sender_psid, response1);
 }
 else if (received_message.attachments) {
