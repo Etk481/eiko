457d0911fff6fe0da36c67a8025d1bc56fa930fc
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-27 16:17:21 +0630

    renew

--- app.js
+++ app.js
@@ -242,7 +242,6 @@ else if (received_message.text == "ရိုးရိုးတံခါးမက
 }
 
 else if (received_message.attachments && botAttachment.doorwtAttachment == true) {
-  console.log('meta data',received_message);
     botAttachment.doorwtAttachment = false;
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
