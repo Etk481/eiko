c2878c8c5944f1eab2caac052b35250ab7d7da92
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-27 17:08:37 +0630

    renew

--- app.js
+++ app.js
@@ -722,7 +722,7 @@ function handlePostback(sender_psid, received_postback) {
              {
                 "type":"postback",
                 "title":"မှာမည်",
-                "payload":"ol"
+                "payload":"wkCh"
               }              
             ]      
           }
@@ -925,7 +925,7 @@ function handlePostback(sender_psid, received_postback) {
               {
                 "type":"postback",
                 "title":"မှာမည်",
-                "payload":"ol"
+                "payload":"dkCh"
               }              
             ]      
           }
@@ -933,7 +933,24 @@ function handlePostback(sender_psid, received_postback) {
       }
     }
    }
-  }else if (payload === 'd1') {
+  }else if (payload ==  'dkCh' || payload ==  'wkCh') {
+     response = {
+        "text":'ဟုတ်ကဲ့ (5"*3")နဲ့ခွေရင် ၁ပေဈေးကတော့ 4000ကျပ်ဖြစ်ပါတယ်။ (5"*2")နဲ့ခွေမယ်ဆိုရင်တော့ ၁ပေဈေးက 3000ကျပ် ဖြစ်ပါတယ်။ မှာယူလိုပါက (5"*3")နဲ့ခွေမှာလား? (5"*3")နဲ့ခွေမှာလား? ရွေးပေးပါခင်ဗျာ။',
+         "quick_replies":[
+        {
+          "content_type":"text",
+          "title":'5"*3"',
+          "payload":"DDu1"
+        },{
+          "content_type":"text",
+          "title":'5"*2"',
+          "payload":"DDu2"
+        }
+      ]
+    }
+}
+
+  else if (payload === 'd1') {
     response = {
     "attachment":{
       "type":"template",
