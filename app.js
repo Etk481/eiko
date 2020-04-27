73e69a53770167faab4ff45734827ae7c14b29ae
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-27 20:05:02 +0630

    renew

--- app.js
+++ app.js
@@ -221,7 +221,18 @@ else if (received_message.text == "တံခါးမကြီးခွေ" || r
       "text":'ဟုတ်ကဲ့အလျားလေးပြောပြပေးပါ။ ဥပမာ - အလျား၆ပေရှိပါက 6 ၊ ၅ပေခွဲရှိပါက 5.5 ဟုပေးပို့ပေးပါ'
     }
   botQuestions.length_f52 = true;  
+}else if (received_message.text == '5"*1.5"') {
+    response = {
+      "text":'ဟုတ်ကဲ့အလျားလေးပြောပြပေးပါ။ ဥပမာ - အလျား၆ပေရှိပါက 6 ၊ ၅ပေခွဲရှိပါက 5.5 ဟုပေးပို့ပေးပါ'
+    }
+  botQuestions.length_sd515 = true;  
+}else if (received_message.text == '4"*1.5"') {
+    response = {
+      "text":'ဟုတ်ကဲ့အလျားလေးပြောပြပေးပါ။ ဥပမာ - အလျား၆ပေရှိပါက 6 ၊ ၅ပေခွဲရှိပါက 5.5 ဟုပေးပို့ပေးပါ'
+    }
+  botQuestions.length_sd515 = true;  
 }
+
 
 else if (received_message.text == "ရိုးရိုးတံခါးမကြီး" || received_message.text == "ရိုးရိုးပြတင်းသစ်ဆံ") {
     response = {"text": 'မှာယူလိုတဲ့ဒီဇိုင်းပုံလေးပို့ပေးပါနော်'   
@@ -354,12 +365,31 @@ else if (received_message.attachments) {
   }
   else if (received_message.text && botQuestions.width_sd515 == true) {
       userAnswers.width_sd515 = received_message.text;
-      let totalFoot_sd515 = 4000 * userAnswers.width_sd515 * userAnswers.length_sd515;
+      let totalFoot_sd515 = 7000 * userAnswers.width_sd515 * userAnswers.length_sd515;
       response = {
         "text":`၁ခုအတွက် ${totalFoot_sd515} ကျပ်ကျပါမယ်။ ဘယ်နှစ်ခုမှာယူလိုပါသလဲ? မှာယူလိုသော Amount ကိုရိုက်ထည့်ပေးပါ။ eg. 1`
       };
       botQuestions.width_sd515 = false;
       botQuestions.quantity_sd515 = true;
+  }  
+  else if (received_message.text && botQuestions.quantity_sd515 == true) {
+      userAnswers.quantity_sd515 = parseInt(received_message.text);
+      let total_sd515 = 7000 * userAnswers.width_sd515 * userAnswers.length_sd515 * userAnswers.quantity_sd515;
+      response = {
+        "text":`စုစုပေါင်း ကျသင့်‌ငွေမှာ ${total_sd515} ဖြစ်ပါတယ်။ မှာယူမှာသေချာပါသလား?`,
+         "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"yes",
+          "payload":"<POSTBACK_PAYLOAD>"
+        },{
+          "content_type":"text",
+          "title":"no",
+          "payload":"<POSTBACK_PAYLOAD>"
+        }
+      ]
+      }
+      botQuestions.quantity_sd515 = false;
   }   
 
 
