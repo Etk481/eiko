ae3c0f875563f7d62be1cd57de158c448eef844c
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-27 09:31:01 +0630

    renew

--- app.js
+++ app.js
@@ -247,9 +247,9 @@ else if (received_message.attachments) {
       }
     }
   }else if (received_message.text && botQuestions.quantity) {
-      userAnswers.quantity = parseDec(received_message.text);
+      userAnswers.quantity = parseInt(received_message.text);
       let total = 30000 * userAnswers.quantity;
-      let orderNumber = Math.floor(Math.random() * 100) + .5;
+      let orderNumber = Math.floor(Math.random() * 100) + 1;
 
       let data = {
         user:"ei thin zar ko",
