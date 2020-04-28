684d6b9041609f1eeb3aec106380b724bd8b0685
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-28 11:39:39 +0630

    renew

--- app.js
+++ app.js
@@ -197,7 +197,11 @@ function handleMessage(sender_psid, received_message) {
                           "type": "postback",
                           "title": "တံခါးပုံများကြည့်မည်",
                           "payload": "look",
-                        }
+                        },{
+                          "type": "postback",
+                          "title": "ဆိုင်ဖုန်းနံပါတ်",
+                          "payload": "ss",
+                        },
                       ],
                     }]
                   }
@@ -651,7 +655,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
 
-  if (payload === 's_address') { 
+  if (payload === 's_address') {
     response = { "text": "ဆိုင်လိပ်စာ (မ/၂၃၉၊ လမ်းမတော်လမ်း၊ ဗိုလ်မင်းရောင်ရပ်ကွက်၊ ‌တပ်ကုန်းမြို့နယ်၊ နေပြည်တော်။)"}
   }else if (payload === 's_Ph') { 
     response = { "text": "ဆိုင်ဖုန်းနံပါတ် (09-799119488, 09-420762842, 09796900093)"}
