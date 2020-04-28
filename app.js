124ec4f6b7069e8668470cc6ed609209ae4fb130
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-28 17:42:40 +0630

    renew

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ function handleMessage(sender_psid, received_message) {
             }
           }
     };
-  let response2 = {"title": "မင်္ဂလာပါ!. NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
+  let response2 = {"text": "မင်္ဂလာပါ!. NS Doors & Windows Shop မှကြိုဆိုပါတယ် ခင်ဗျာ"};
   let response3 = {"text":'လူကြီးမင်းသိလိုသည်များကို အောက်ပါခလုတ်များကိုနှိပ်၍ သိရှိနိုင်ပါတယ်...NS Doors & Windows Shop မှ ကျေးဇူးအထူးတင်ရှိပါတယ်ခင်ဗျာ...'};
   let response4 = { "attachment": {
                   "type": "template",
