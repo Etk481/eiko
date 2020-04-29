29c013fb32806da2de0622b6c03b7b3dc2f7d21c
eithinzarko <eithinzarko.tk@gmail.com>
2020-04-29 15:46:38 +0630

    renew

--- app.js
+++ app.js
@@ -295,7 +295,7 @@ else if (received_message.text == "ရိုးရိုးတံခါးမက
   else if (received_message.attachments && botQuestions.image == true) {
     userAnswers.image = received_message.attachment;
     // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
+    let attachment_url = userAnswers.image[0].payload.url;
     response = {
       "attachment": {
         "type": "template",
