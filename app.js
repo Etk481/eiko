436af0ad8647453d2f06724562ce29e89500f9eb
eithinzarko <eithinzarko.tk@gmail.com>
2020-05-01 13:59:57 +0630

    renew

--- app.js
+++ app.js
@@ -327,21 +327,6 @@ else if (received_message.text == "ရိုးရိုးတံခါးမက
   callSend(sender_psid, response1).then(()=>{
       return callSend(sender_psid, response2);
     });   
-}else if (received_message.payload == "shareYes") {
-    response = {
-        "text":'ဟုတ်ကဲ့ (5"*1.5")နဲ့ခွေရင် ၁ပေဈေးကတော့ 7000ကျပ်ဖြစ်ပါတယ်။ (4"*1.5")နဲ့ခွေမယ်ဆိုရင်တော့ ၁ပေဈေးက 6700ကျပ် ဖြစ်ပါတယ်။ မှာယူလိုပါက (5"*1.5")နဲ့ခွေမှာလား? (4"*1.5")နဲ့ခွေမှာလား? ရွေးပေးပါခင်ဗျာ။',
-         "quick_replies":[
-        {
-          "content_type":"text",
-          "title":'5"*1.5"',
-          "payload":"d1Ch1p"
-        },{
-          "content_type":"text",
-          "title":'4"*1.5"',
-          "payload":"d1Ch2p"
-        }
-      ]
-    }
 }
 
 
