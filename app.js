032389b2335cb02ae1241344b1b69d24d5a042cb
eithinzarko <eithinzarko.tk@gmail.com>
2020-05-03 11:50:51 +0630

    renew

--- app.js
+++ app.js
@@ -936,7 +936,7 @@ if (received_message.text == "Yes!") {
         total_price_515: total_price_515,
       }
 
-      db.collection('order_D&W_wood515').doc().set(data);
+      db.collection('order_DW_wood515').doc().set(data);
 
     let response1 = { "text":'မှာယူမှုအောင်မြင်ပါသည်။'};
     let response2 = { "text" : 'လူကြီးမင်းမှာယူထားသောအော်ဒါကို ပြုလုပ်ပီးပါက လူကြီးမင်းဆီသို့ ဖုန်းဆက်၍‌ေသာ်လည်း‌ေကာင်း၊ စာတိုပေးပို့၍‌ေသာ်လည်း‌ေကာင်း အကြောင်းကြားပေးပါမည်။ ဝယ်ယူမှုအတွက်ကျေးဇူးအထူးဘဲတင်ရှိပါတယ်ခင်ဗျာ။'};
